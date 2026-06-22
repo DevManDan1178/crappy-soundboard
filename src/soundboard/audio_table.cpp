9555a709524eaf18672010c326bff8807bc12894
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-22 16:52:38 -0400

    + audio table class

---
+++ src/soundboard/audio_table.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <optional>
+#include "audio/audio.hpp"
+#include "soundboard/audio_table.hpp"
+
+int AudioTable::CreateSubTable() {
+    std::vector<Audio> newSubTable;
+    data.push_back(newSubTable);
+    return size();
+}
+
+bool AudioTable::AddAudio(Audio audio, size_t tableIndex) {
+    if (tableIndex >= size()) {
+        std::cout << "Attempted insert into invalid index of AudioTable (" << tableIndex << ")" << std::endl;
+        return false;
+    }
+    std::vector<Audio>& subTable = data[tableIndex];
+    if (subTable.size() >= MAX_SUBTABLE_SIZE) {
+        std::cout << "Attempted insert into already full index of AudioTable (" << tableIndex << ")" << std::endl;
+        return false;
+    }
+    subTable.push_back(audio);
+    std::cout << "Successful insert into AudioTable at index: (" << tableIndex << ")" << std::endl;
+    return true;
+}
+
+
+bool AudioTable::RemoveAudio(size_t tableIndex, size_t index) {
+    if (tableIndex >= size()) {
+        std::cout << "Attempted removal from invalid index of AudioTable (" << tableIndex << ")" << std::endl;
+        return false;
+    }
+    std::vector<Audio>& subTable = data[tableIndex];
+    if (index >= subTable.size()) {
+        std::cout << "Attempted removal from invalid subindex of AudioTable (" << tableIndex << ", " << index << ")" << std::endl;
+        return false;
+    }
+    subTable.erase(subTable.begin() + index);
+    std::cout << "Successful removal from AudioTable at table index (" << tableIndex << ") and index (" << index << ")" << std::endl;
+    return true;
+}
+
+
+std::vector<Audio> AudioTable::GetAudiosAtIdx(size_t tableIndex) {
+    if (tableIndex >= size()) {
+        std::cout << "Attempted query of invalid index of AudioTable (" << tableIndex << ")" << std::endl;
+        return {};
+    }
+    return data[tableIndex];
+}
+
+
+std::optional<Audio> AudioTable::GetAudio(size_t tableIndex, size_t index) {
+    std::vector<Audio>& subTable = GetAudiosAtIdx(tableIndex);
+    std::optional<Audio> audio;
+    if (index >= subTable.size()) {
+        return audio;
+    }
+    return subTable[index];
+}
---
+++ src/soundboard/audio_table.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <vector>
+#include "audio/audio.hpp"
+#include <optional>
+
+/**
+ * @brief table for a soundboard emote wheel: 2D vector (first the wheel's index, then the index in the wheel)
+ */
+class AudioTable {
+private:
+    std::vector<std::vector<Audio>> data;
+
+public:
+    constexpr static const int MAX_SUBTABLE_SIZE = 5;
+    
+    /**
+     * @brief adds an audio to the table index if possible
+     * @param tableIndex index of the table [from 0]
+     * @return if it was successful or not
+     */
+    bool AddAudio(Audio audio, size_t tableIndex);
+
+    /**
+     * @brief appends an empty subtable to the table
+     * @return index of the new subtable [from 0]
+     */
+    int CreateSubTable();
+
+    /**
+     * @brief removes the audio at the corresponding position
+     * @param tableIndex index of the table [from 0]
+     * @param index index in the table [from 0]
+     * @return if it was successful or not
+     */
+    bool RemoveAudio(size_t tableIndex, size_t index);
+    
+    /**
+     * @brief gets the list of audios at the table index
+     * @param tableIndex index of the table [from 0]
+     * @return list of audios at the table index (copy)
+     */
+    std::vector<Audio> GetAudiosAtIdx(size_t tableIndex);
+
+    /**
+     * @brief gets the audio at the corresponding position
+     * @param tableIndex index of the table [from 0]
+     * @param index index in the table [from 0]
+     * @return the audio (optional, copy)
+     */
+    std::optional<Audio> GetAudio(size_t tableIndex, size_t index);
+
+    /**
+     * @brief gets size of table (amount of audio lists)
+    */
+    size_t size(){
+        return data.size();
+    }
+};
