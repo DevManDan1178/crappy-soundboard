915424064f96c62fc71833f2b775f12266cc4566
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-26 01:00:05 -0400

    + functions from inner audio table

--- src/soundboard/soundboard.cpp
+++ src/soundboard/soundboard.cpp
@@ -2,11 +2,22 @@
 #include <optional>
 
 
-bool Soundboard::PlayAudio(int tableIndex, int index) {
+bool Soundboard::PlayAudio(unsigned int tableIndex, unsigned int index) {
     std::optional<Audio> audio = audioTable.GetAudio(tableIndex, index);
     if (audio.has_value()) {
         audioManager.PlayAudio(*audio);
         return true;
     }
     return false;
+}
+
+unsigned int Soundboard::AudioTableSize() {
+    return audioTable.size();
+}
+
+std::vector<Audio>* Soundboard::GetAudiosAtIndex(unsigned int index) {
+    if (index >= AudioTableSize()) {
+        return {};
+    }
+    return audioTable.GetAudiosAtIdx(index);
 }
--- src/soundboard/soundboard.hpp
+++ src/soundboard/soundboard.hpp
@@ -18,5 +18,19 @@ public:
      * @param index index in the table [from 0]
      * @return if it was successful or not
      */
-    bool PlayAudio(int tableIndex, int index);
+    bool PlayAudio(unsigned int tableIndex, unsigned int index);
+
+
+    /**
+     * @brief gets the size of the audio table
+     * @return size of the audio table
+     */
+    unsigned int AudioTableSize();
+
+    /**
+     * @brief gets the list of audios at the index
+     * @param index the index to query
+     * @return list of audios at the index (pointer)
+     */
+    std::vector<Audio>* GetAudiosAtIndex(unsigned int index);
 };
